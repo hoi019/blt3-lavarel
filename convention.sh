71e6616614204fe63200ddcda264bc4a854cb9cb
nguyenquochoi <134429276+hoideptraivodoibadao123@users.noreply.github.com>
2024-03-10 18:44:15 +0700

    day du an

---
+++ convention.sh
@@ -0,0 +1,7 @@
+echo 'test prs-2'
+phpcs --standard=PSR2 --sniffs=Generic.PHP.LowerCaseConstant-tests
+phpcs --standard=PSR2 app/Traits
+phpcs --standard=PSR2 app/Models
+phpcs --standard=PSR2 app/Services
+phpcs --standard=PSR2 app/Repositories
+phpcs --standard=PSR2 app/Http/Controllers
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.layouts.app');
+});
+
+Route::get('/home', function () {
+    return view('client.layouts.app');
+});
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeControllers::class, 'index'])->name('home');
